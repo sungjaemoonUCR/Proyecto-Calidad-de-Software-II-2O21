808109809e9ae5f2f146508b8cc365d536ac9454
Steve Moon <56247331+sungjaemoonUCR@users.noreply.github.com>
2021-11-04 10:10:23 -0600

    Delete Proyecto_Inge_Bases_Web/Scripts directory

--- Proyecto_Inge_Bases_Web/Scripts/ValidacionRegistro.js
+++
@@ -1,8 +0,0 @@
-﻿
-    function equalPasswords(input) {
-                    if (input.value != document.getElementById('Contraseña').value) {
-        input.setCustomValidity('Las contraseñas deben ser iguales');
-                    } else {
-        input.setCustomValidity('');
-                    }
-                }
